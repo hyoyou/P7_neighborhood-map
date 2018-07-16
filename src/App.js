ce09424505683edd061b893b6d78657f371e8de4
Heather You <heatherhyou@gmail.com>
2018-07-16 17:36:48 -0400

    add map through asynchronous code injection

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-import GoogleApiComponent from '../node_modules/google-maps-react/dist/GoogleApiComponent';
-import { Map, Marker, InfoWindow } from 'google-maps-react';
 import './App.css';
-// import Marker from './Marker';
 import { restaurants } from './markers';
 
 class App extends Component {
@@ -11,36 +8,21 @@ class App extends Component {
 
     this.state = {
       restaurants: restaurants,
-      showingInfoWindow: false,
       activeMarker: {},
       selectedPlace: {}
     }
   }
 
-  // markerInfo = (event) => {
-  //   InfoWindow.open(map, this)
-
-  //   this.setState({
-  //     showingInfoWindow: true,
-  //     selectedPlace: event.name
-  //   })
-  // }
-
-  onMarkerClick = (props, marker, e) => {
-    this.setState({
-      selectedPlace: props,
-      activeMarker: marker,
-      showingInfoWindow: true
-    });
+  componentDidMount() {
+    window.initMap = this.initMap;
+    createMapLink(`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAP_API_KEY}&callback=initMap`);
   }
 
-  onMapClick = (props) => {
-    if (this.state.showingInfoWindow) {
-      this.setState({
-        showingInfoWindow: false,
-        activeMarker: null
-      });
-    }
+  initMap() {
+    let map = new window.google.maps.Map(document.getElementById('map'), {
+      zoom: 16,
+      center: {lat: 28.4753086, lng: -81.4693343}
+    })
   }
   
   render() {
@@ -52,22 +34,26 @@ class App extends Component {
     return (
       <div>
         <div id="map" style={style}>
-          <Map google={this.props.google}>
-            {restaurants.map((restaurant, index) => (
-              <div key={index}>
-                <Marker onClick={this.markerInfo} name={restaurant.name} position={restaurant.pos} />
-                <InfoWindow marker={this.state.activeMarker} visible={this.state.showingInfoWindow} mapCenter={restaurant.pos} >
-                  <h1>Restaurant</h1>
-                </InfoWindow>
-              </div>
-            ))}
-          </Map>
+          
         </div>
       </div>
     );
   }
 }
 
-export default GoogleApiComponent({
-  apiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY
-})(App);
+// Add Google Map to page dynamically through asynchronous code injection
+function createMapLink(url) {
+  let script = window.document.createElement('script');
+  script.src = url;
+  script.async = true;
+  
+  script.onerror = function() {
+    document.write("Error loading Google Maps")
+  }
+
+  let parent = window.document.getElementsByTagName('script')[0];
+  parent.appendChild(script);
+}
+
+export default App;
+
