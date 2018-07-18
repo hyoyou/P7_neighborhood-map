ef6a702f92b8ec081e54bed2750759311dd15780
Heather You <heatherhyou@gmail.com>
2018-07-18 12:32:29 -0400

    remove unused functions

--- src/helpers.js
+++ src/helpers.js
@@ -3,9 +3,7 @@
 export const lazyLoad = function(url) {
     let script = window.document.createElement('script');
     script.defer = true;
-    script.onload = "this.checkError()";
-    script.onerror = "this.gm_authFailure()";
-  
     script.src = url;
+    
     document.body.appendChild(script);     
 }
