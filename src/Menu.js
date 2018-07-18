61c0d483df86a3242c21677db4d5d869a08f22b9
Heather You <heatherhyou@gmail.com>
2018-07-18 12:42:15 -0400

    add aria-label to hamburger button

--- src/Menu.js
+++ src/Menu.js
@@ -50,7 +50,7 @@ export default class Menu extends Component {
 
         return (
             <nav className="menu">
-                <div id="list-toggle" className="hamburger" onClick={this.toggleShow} onKeyPress={this.toggleShow} tabIndex="0" role="button">
+                <div id="list-toggle" className="hamburger" onClick={this.toggleShow} onKeyPress={this.toggleShow} tabIndex="0" role="button" aria-label="Toggle navigation">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                         <path d="M2 6h20v3H2zm0 5h20v3H2zm0 5h20v3H2z"></path>
                     </svg>
